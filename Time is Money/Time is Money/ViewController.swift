8bc151d6633afd9b71423a55c5c74ce83c16618c
Victor Melo <victor.contas@outlook.com>
2019-10-08 16:12:17 -0300

    Created Xcode project

---
+++ Time is Money/Time is Money/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Time is Money
+//
+//  Created by Victor Melo on 08/10/19.
+//  Copyright © 2019 Victor Melo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
