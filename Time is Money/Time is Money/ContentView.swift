16ee13b63569e3de9d9f1f7438da94444afeca78
Victor <victor.contas@outlook.com>
2019-10-10 08:16:06 -0300

    Re added project with SwiftUI

---
+++ Time is Money/Time is Money/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Time is Money
+//
+//  Created by Victor S Melo on 10/10/19.
+//  Copyright © 2019 Victor S Melo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Time is Money/Time is MoneyTests/Time_is_MoneyTests.swift
@@ -0,0 +1,34 @@
+//
+//  Time_is_MoneyTests.swift
+//  Time is MoneyTests
+//
+//  Created by Victor S Melo on 10/10/19.
+//  Copyright © 2019 Victor S Melo. All rights reserved.
+//
+
+import XCTest
+@testable import Time_is_Money
+
+class Time_is_MoneyTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
