a5ecaa751e939fc8d6205d0bf35b71c616d1c0a0
Victor Melo <victormelo@Computador.local>
2021-07-13 21:23:06 -0300

    Views refactoring

--- Time is Money/Time is Money/Flows/Home/Views/MainView.swift
+++ Time is Money/Time is Money/Flows/Home/Views/MainView.swift
@@ -20,20 +20,12 @@ struct MainView: View {
     @State var priceAsSeconds: TimeInterval = 100.0
     @State var isShowingEditView = false
     @State var isKeyboardVisible = false
-    @State var isShowingAlert: Bool = false
     @State var isMonetaryValueZero: Bool = false
     
     var body: some View {
-        //        let formattedValue = Formatter.currency.string(from: viewModel.appState.currentPrice) ?? "?"
-        
-        //        let priceBinding = Binding(
-        //            get: { viewModel.appState.currentPrice.decimalValue },
-        //            set: { viewModel.appState.currentPrice = NSDecimalNumber(decimal: $0) }
-        //        )
-        
-        return VStack(alignment: .center, spacing: 0) {
-            HomeNavigationBarView(isKeyboardVisible: $isKeyboardVisible,
-                                  isShowingEditView: $isShowingEditView)
+        VStack(alignment: .center, spacing: 0) {
+            HomeNavigationBarView(isKeyboardVisible: $isKeyboardVisible, isShowingEditView: $isShowingEditView)
+
             HomeHeaderView(user: $appState.user, isKeyboardVisible: $isKeyboardVisible, isMonetaryValueZero: $isMonetaryValueZero, priceAsSeconds: $priceAsSeconds)
             
             Spacer()
@@ -45,32 +37,12 @@ struct MainView: View {
             InputSectionView(priceBinding: $appState.currentPrice, isKeyboardVisible: isKeyboardVisible)
             
         }
-        .navigationBarBackButtonHidden(true)
-        .navigationBarTitle("")
         .navigationBarHidden(true)
         .sheet(isPresented: $isShowingEditView) {
             EditView().environmentObject(appState)
         }
-        .frame(width: UIScreen.main.bounds.width, alignment: .center)
-        .onAppear() {
-            if self.appState.user.isOnboardingCompleted == false {
-                self.appState.user.isOnboardingCompleted.toggle()
-            }
-            
-            if self.appState.avatarId == "male2-deprecated" {
-                self.isShowingAlert = true
-                self.appState.avatarId = "male2"
-            }
-            
-            self.interactors.mainInteractor.saveAppState()
-        }.alert(isPresented: $isShowingAlert) {
-            return Alert(title: Text(R.string.localizable.yayUpdate()), message: Text(R.string.localizable.nowYouCanSelectADifferentAvatarGoToSettingsScreenToSelectYours()), dismissButton: Alert.Button.default(Text("Ok")))
-        }
-        .gesture(TapGesture()
-                    .onEnded {
-                        self.hideKeyboard()
-                    }
-        )
+        .onTapGesture(perform: hideKeyboard)
+        .withBackground()
     }
 }
 
--- Time is Money/Time is Money/Flows/Onboarding/Views/OnboardingIncomeView.swift
+++ Time is Money/Time is Money/Flows/Onboarding/Views/OnboardingIncomeView.swift
@@ -15,6 +15,13 @@ struct OnboardingIncomeView: View {
 
     @State private var offsetValue: CGFloat = 0.0
     
+    var incomeBinding: Binding<NSDecimalNumber> {
+        Binding(
+            get: { self.appState.user.monthlySalary },
+            set: { self.appState.user.monthlySalary = $0 }
+        )
+    }
+    
     init() {
         UITableView.appearance().tableHeaderView = UIView(frame: CGRect(x: 0, y: 0, width: 0, height: Double.leastNonzeroMagnitude))
         UITableView.appearance().tableFooterView = UIView(frame: CGRect(x: 0, y: 0, width: 0, height: Double.leastNonzeroMagnitude))
@@ -22,68 +29,65 @@ struct OnboardingIncomeView: View {
     }
     
     var body: some View {
-        
-        let incomeBinding = Binding(
-            get: { self.appState.user.monthlySalary },
-            set: { self.appState.user.monthlySalary = $0 }
-        )
-        
-        return Group {
-            VStack {
-                
-                Spacer()
-                
-                HStack {
-                    Text(R.string.localizable.whatIsYourIncome())
-                        .padding(.top, 20)
-                        .font(config.font.bold(size: .title).swiftUIFont)
-                        .foregroundColor(config.color.complementaryColor.swiftUIColor)
-                        .frame(maxWidth: .infinity, maxHeight:80)
-                }
-                
-                Spacer()
-                
-                HStack {
-                    Image("Money_img")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: UIScreen.main.bounds.width-120, alignment: .center)
-                        .frame(minHeight: 100, alignment: .center)
-                        .animation(.none)
-                }
-                
-                Spacer()
-                
-                HStack {
-                    CurrencyField(incomeBinding, placeholder: "Income")
-                        .frame(width: UIScreen.main.bounds.width, height: 60, alignment: .center)
-                }
-                
-                HStack {
-                    Text("per month")
-                        .font(config.font.light(size: .body).swiftUIFont)
-                        .foregroundColor(config.color.complementaryColor.swiftUIColor)
-                }
-                
-                Spacer()
-                
-                HStack {
-                    NavigationLink(destination: MainView().environmentObject(self.appState)) {
-                        Text("Finish")
-                            .font(config.font.semibold(size: .body).swiftUIFont)
-                            .frame(width: 200, height: 50, alignment: .center)
-                            .background(config.color.complementaryColor.swiftUIColor)
-                            .foregroundColor(config.color.primaryColor.swiftUIColor)
-                            .cornerRadius(5)
-                    }
-                }
-                
-                Spacer()
-            }
+        VStack {
+            Spacer()
+            
+            Text(R.string.localizable.whatIsYourIncome())
+                .padding(.top, 20)
+                .font(config.font.bold(size: .title).swiftUIFont)
+                .foregroundColor(config.color.complementaryColor.swiftUIColor)
+                .frame(maxWidth: .infinity, maxHeight:80)
+            
+            Spacer()
+            
+            Image("Money_img")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: UIScreen.main.bounds.width-120, alignment: .center)
+                .frame(minHeight: 100, alignment: .center)
+                .animation(.none)
+            
+            Spacer()
+            
+            CurrencyField(incomeBinding, placeholder: "Income")
+                .frame(width: UIScreen.main.bounds.width, height: 60, alignment: .center)
+            
+            Text("per month")
+                .font(config.font.light(size: .body).swiftUIFont)
+                .foregroundColor(config.color.complementaryColor.swiftUIColor)
+            
+            Spacer()
+            
+            FinishButton()
+            
+            Spacer()
         }
         .withBackground()
-        .navigationBarTitle("")
         .navigationBarHidden(true)
+    }
+}
+
+struct FinishButton: View {
+    
+    @EnvironmentObject var appState: AppState
+    @Environment(\.interactors) var interactors: InteractorsContainer
+    @State private var action: Int? = 0
+    
+    var body: some View {
+        VStack {
+            NavigationLink(destination: MainView().environmentObject(appState), tag: 1, selection: $action) { EmptyView() }
+            Text("Finish")
+                .font(config.font.semibold(size: .body).swiftUIFont)
+                .frame(width: 200, height: 50, alignment: .center)
+                .background(config.color.complementaryColor.swiftUIColor)
+                .foregroundColor(config.color.primaryColor.swiftUIColor)
+                .cornerRadius(5)
+                .onTapGesture {
+                    appState.user.isOnboardingCompleted = true
+                    interactors.mainInteractor.saveAppState()
+                    action = 1
+                }
+        }
     }
 }
 
